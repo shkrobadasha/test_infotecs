32c96af10e22705d0824c034265e65a1562a4dcb
Shkroba Daria <shkrobadaria@mail.ru>
2025-07-22 16:31:39 +0300

    add sorting without styles

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,8 +3,49 @@ import React, { useEffect, useState } from "react"
 const Table = () => {
     const [users, setUsers] = useState([])
     const [isLoading, setLoading] = useState(false)
+    const [sortConfig, setSortConfig] = useState({sortByfirstName: 1, 
+        sortBymiddleName: 1,
+        sortBylastName: 1,
+        sortByage:1,
+        sortBygender: 1,
+        sortByphone: 1,
+    })
+
+    const sortHandler = async (sortParametr) => {
+        let sort = sortConfig[`sortBy${sortParametr}`]
+        setSortConfig({...sortConfig, [`sortBy${sortParametr}`]: sort < 2 ? sort+= 1: sort-= 2})
+
+        let url
+        switch (sortConfig[`sortBy${sortParametr}`]){
+            case 0:
+                url = `https://dummyjson.com/users`
+                break
+            case 1: 
+                url = `https://dummyjson.com/users?sortBy=${sortParametr}&order=asc`
+                break
+            case 2: 
+            //desc
+            url = `https://dummyjson.com/users?sortBy=${sortParametr}&order=desc`
+            break
+        }
+        
+        try{
+            const response = await fetch(url)
+            setLoading(true)
+            if(!response.ok){
+                throw new Error(`HTTP error: ${response.status}`)
+            }
+            const sortData = await response.json()
+            setLoading(false)
+            setUsers(sortData.users)
+
+        } catch (error) {
+            console.log(`Failed to complete the request: ${error.message}`)
+        }
+    }
 
     const getUserData = async () => {
+
         try {
             const response = await fetch('https://dummyjson.com/users')
             setLoading(true)
@@ -14,6 +55,7 @@ const Table = () => {
             const data = await response.json()
             setLoading(false)
             setUsers(data.users)
+            console.log(data.users)
         } catch (error){
             console.log(`Failed to complete the request: ${error.message}`)
         }
@@ -27,12 +69,12 @@ const Table = () => {
         <table className = 'table without style'> 
             <thead>
                 <tr>
-                    <th>Фамилия</th>
-                    <th>Имя</th>
-                    <th>Отчество</th>
-                    <th>Возраст</th>
-                    <th>Пол</th>
-                    <th>Номер телефона</th>
+                    <th onClick = {() => sortHandler('lastName')} >Фамилия</th>
+                    <th onClick = {() => sortHandler('firstName')}>Имя</th>
+                    <th onClick = {() => sortHandler('middleName')}>Отчество</th>
+                    <th onClick = {() => sortHandler('age')}>Возраст</th>
+                    <th onClick = {() => sortHandler('gender')}>Пол</th>
+                    <th onClick = {() => sortHandler('phone')}>Номер телефона</th>
                     <th>Email</th>
                     <th>Страна</th>
                     <th>Город</th>
